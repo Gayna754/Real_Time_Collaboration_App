38a25c0bf241844705819c886c2414cad5c64f36
Gayna Chawla <gaynachawla@gmail.com>
2026-05-25 23:43:54 +0530

    Add file upload preview with append/replace editor support

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -14,7 +14,6 @@ import {
   useParams,
 } from "react-router-dom";
 
-
 const EditorPage = () => {
   const [lang, setLang] = useRecoilState(language);
   const [them, setThem] = useRecoilState(cmtheme);
@@ -32,7 +31,7 @@ const EditorPage = () => {
   const fileInputRef = useRef(null);
   const editorInstanceRef = useRef(null);
 
-useEffect(() => {
+  useEffect(() => {
     const init = async () => {
       socketRef.current = await initSocket();
       socketRef.current.on("connect_error", (err) => handleErrors(err));
@@ -79,7 +78,7 @@ useEffect(() => {
       socketRef.current.off(ACTIONS.DISCONNECTED);
       socketRef.current.disconnect();
     };
-  }, []);}
+  }, []);
 
   async function copyRoomId() {
     try {
@@ -98,6 +97,7 @@ useEffect(() => {
   if (!location.state) {
     return <Navigate to="/" />;
   }
+
   function handleFileUpload(event) {
     console.log("hello");
     const file = event.target.files[0];
@@ -125,6 +125,7 @@ useEffect(() => {
       code: newCode,
     });
   };
+
   const handleAppendCode = () => {
   const currentCode = codeRef.current || "";
 
@@ -141,11 +142,172 @@ useEffect(() => {
     updateEditorCode(fileContent);
     setFilePreview(false);
     resetFileInput();
-    
   };
 
-  export default EditorPage;
 
 
+  return (
+    <div className="mainWrap">
+      <div className="aside">
+        <div className="asideInner">
+          <div className="logo">
+            <img className="logoImage" src="/logo.png" alt="logo" />
+          </div>
+          <h3>Connected</h3>
+          <div className="clientsList">
+            {clients.map((client) => (
+              <Client key={client.socketId} username={client.username} />
+            ))}
+          </div>
+        </div>
 
- 
+        <input type="file" accept=".js,.py,.java,.cpp,.c,.txt,.html,.css" style={{ display: "none" }} id="fileUpload" onChange={handleFileUpload} ref={fileInputRef} />
+        <button className="uploadFileBtn" onClick={() => document.getElementById("fileUpload").click()}>
+          Upload File
+        </button>
+        {
+          filePreview && <FilePreview 
+            setFilePreview={setFilePreview}
+            fileContent={fileContent}
+            resetFileInput={resetFileInput}
+            onAppend={handleAppendCode}
+            onReplace={handleReplaceCode}/>
+        }
+
+        <label>
+          Select Language:
+          <select
+            value={lang}
+            onChange={(e) => {
+              setLang(e.target.value);
+              window.location.reload();
+            }}
+            className="seLang"
+          >
+            <option value="clike">C / C++ / C# / Java</option>
+            <option value="css">CSS</option>
+            <option value="dart">Dart</option>
+            <option value="django">Django</option>
+            <option value="dockerfile">Dockerfile</option>
+            <option value="go">Go</option>
+            <option value="htmlmixed">HTML-mixed</option>
+            <option value="javascript">JavaScript</option>
+            <option value="jsx">JSX</option>
+            <option value="markdown">Markdown</option>
+            <option value="php">PHP</option>
+            <option value="python">Python</option>
+            <option value="r">R</option>
+            <option value="rust">Rust</option>
+            <option value="ruby">Ruby</option>
+            <option value="sass">Sass</option>
+            <option value="shell">Shell</option>
+            <option value="sql">SQL</option>
+            <option value="swift">Swift</option>
+            <option value="xml">XML</option>
+            <option value="yaml">yaml</option>
+          </select>
+        </label>
+
+        <label>
+          Select Theme:
+          <select
+            value={them}
+            onChange={(e) => {
+              //   setCode(codeRef.current);
+              setThem(e.target.value);
+              //   window.location.reload();
+            }}
+            className="seLang"
+          >
+            <option value="default">default</option>
+            <option value="3024-day">3024-day</option>
+            <option value="3024-night">3024-night</option>
+            <option value="abbott">abbott</option>
+            <option value="abcdef">abcdef</option>
+            <option value="ambiance">ambiance</option>
+            <option value="ayu-dark">ayu-dark</option>
+            <option value="ayu-mirage">ayu-mirage</option>
+            <option value="base16-dark">base16-dark</option>
+            <option value="base16-light">base16-light</option>
+            <option value="bespin">bespin</option>
+            <option value="blackboard">blackboard</option>
+            <option value="cobalt">cobalt</option>
+            <option value="colorforth">colorforth</option>
+            <option value="darcula">darcula</option>
+            <option value="duotone-dark">duotone-dark</option>
+            <option value="duotone-light">duotone-light</option>
+            <option value="eclipse">eclipse</option>
+            <option value="elegant">elegant</option>
+            <option value="erlang-dark">erlang-dark</option>
+            <option value="gruvbox-dark">gruvbox-dark</option>
+            <option value="hopscotch">hopscotch</option>
+            <option value="icecoder">icecoder</option>
+            <option value="idea">idea</option>
+            <option value="isotope">isotope</option>
+            <option value="juejin">juejin</option>
+            <option value="lesser-dark">lesser-dark</option>
+            <option value="liquibyte">liquibyte</option>
+            <option value="lucario">lucario</option>
+            <option value="material">material</option>
+            <option value="material-darker">material-darker</option>
+            <option value="material-palenight">material-palenight</option>
+            <option value="material-ocean">material-ocean</option>
+            <option value="mbo">mbo</option>
+            <option value="mdn-like">mdn-like</option>
+            <option value="midnight">midnight</option>
+            <option value="monokai">monokai</option>
+            <option value="moxer">moxer</option>
+            <option value="neat">neat</option>
+            <option value="neo">neo</option>
+            <option value="night">night</option>
+            <option value="nord">nord</option>
+            <option value="oceanic-next">oceanic-next</option>
+            <option value="panda-syntax">panda-syntax</option>
+            <option value="paraiso-dark">paraiso-dark</option>
+            <option value="paraiso-light">paraiso-light</option>
+            <option value="pastel-on-dark">pastel-on-dark</option>
+            <option value="railscasts">railscasts</option>
+            <option value="rubyblue">rubyblue</option>
+            <option value="seti">seti</option>
+            <option value="shadowfox">shadowfox</option>
+            <option value="solarized">solarized</option>
+            <option value="the-matrix">the-matrix</option>
+            <option value="tomorrow-night-bright">tomorrow-night-bright</option>
+            <option value="tomorrow-night-eighties">
+              tomorrow-night-eighties
+            </option>
+            <option value="ttcn">ttcn</option>
+            <option value="twilight">twilight</option>
+            <option value="vibrant-ink">vibrant-ink</option>
+            <option value="xq-dark">xq-dark</option>
+            <option value="xq-light">xq-light</option>
+            <option value="yeti">yeti</option>
+            <option value="yonce">yonce</option>
+            <option value="zenburn">zenburn</option>
+          </select>
+        </label>
+
+        <button className="btn copyBtn" onClick={copyRoomId}>
+          Copy ROOM ID
+        </button>
+        <button className="btn leaveBtn" onClick={leaveRoom}>
+          Leave
+        </button>
+      </div>
+
+      <div className="editorWrap">
+        <Editor
+          ref={editorInstanceRef}
+          socketRef={socketRef}
+          roomId={roomId}
+          onCodeChange={(code) => {
+            console.log("on code change" + code);
+            codeRef.current = code;
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default EditorPage;
