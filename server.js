c025af95ad13ed0959a00ad260e5f6f6779b6395
Gayna Chawla <gaynachawla@gmail.com>
2026-05-30 23:23:03 +0530

    Path express

--- server.js
+++ server.js
@@ -27,3 +27,40 @@ function getAllConnectedClients(roomId) {
         }
     );
 }
+
+io.on('connection', (socket) => {
+    console.log('socket connected', socket.id);
+
+    socket.on(ACTIONS.JOIN, ({roomId, username}) => {
+        userSocketMap[socket.id] = username;
+        socket.join(roomId);
+        const clients = getAllConnectedClients(roomId);
+        clients.forEach(({socketId}) => {
+            io.to(socketId).emit(ACTIONS.JOINED, {
+                clients,
+                username,
+                socketId: socket.id,
+            });
+        });
+    });
+
+    socket.on(ACTIONS.CODE_CHANGE, ({roomId, code}) => {
+        socket.in(roomId).emit(ACTIONS.CODE_CHANGE, {code});
+    });
+
+    socket.on(ACTIONS.SYNC_CODE, ({socketId, code}) => {
+        io.to(socketId).emit(ACTIONS.CODE_CHANGE, {code});
+    });
+
+    socket.on('disconnecting', () => {
+        const rooms = [...socket.rooms];
+        rooms.forEach((roomId) => {
+            socket.in(roomId).emit(ACTIONS.DISCONNECTED, {
+                socketId: socket.id,
+                username: userSocketMap[socket.id],
+            });
+        });
+        delete userSocketMap[socket.id];
+        socket.leave();
+    });
+});
