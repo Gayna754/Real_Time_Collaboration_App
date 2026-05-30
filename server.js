787404ce395869d594b224162b311e2e6da6111d
Gayna Chawla <gaynachawla@gmail.com>
2026-05-30 23:20:36 +0530

    Path express

--- server.js
+++ server.js
@@ -9,3 +9,8 @@ const ACTIONS = require('./src/actions/Actions');
 
 const server = http.createServer(app);
 const io = new Server(server);
+
+app.use(express.static('build'));
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
